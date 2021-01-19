ca0259a9c25d83597641cfcb5fb3a3e843b866e5
Thomas Rosenbaugh <tomgrosenbaugh@gmail.com>
2021-01-18 17:49:13 -0700

    Added functionality to use the satisfaction_survey class object

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "turtles"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
 
 debug = DebugToolbarExtension(app)
 
@@ -13,6 +15,34 @@ responses = []
 
 @app.route("/")
 def start_survey():
-    return render_template("templates/start_survey.html", title=satisfaction_survey.title, instructions=satisfaction_survey.instructions)
-    
+    return render_template("start_survey.html", title=satisfaction_survey.title, instructions=satisfaction_survey.instructions)
 
+@app.route("/questions/<int:question_num>")
+def ask_question(question_num):
+    # Check to make sure the user is accessing the correct question
+    if question_num != len(responses):
+        flash("You are trying to access in invalid page")
+        return redirect(f"/questions/{len(responses)}")
+
+    question = satisfaction_survey.questions[question_num].question
+    choices = satisfaction_survey.questions[question_num].choices
+
+    return render_template("questions.html", question=question, choices=choices)
+
+@app.route("/answer", methods=["POST"])
+def handle_answer():
+    # Get the answer from the user and store it in responses
+    answer = request.form['answer']
+    responses.append(answer)
+
+    #Check to see if the amount of questions has run out, if so redirect to thank you page
+    if len(responses) == len(satisfaction_survey.questions):
+        return redirect("/complete")
+    else:
+        return redirect(f"/questions/{len(responses)}")
+    
+@app.route("/complete")
+def thank_you():
+    return render_template("thank_you.html")
+
+    
